80b4c98960979d9519d9e08e60f49c79e00264b8
Grant Reed <grant.davis.reed@gmail.com>
2018-10-25 19:11:19 -0700

    add employer checkbox

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to Easy Apply!"
       redirect_to @user
     else
       render 'new'
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :email, :employer,
+                                   :password, :password_confirmation)
     end
 end
